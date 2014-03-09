6ff794968697d9ec6a6d9ab3e40a801ef73e3507
Dave <dahaden@gmail.com>
2014-03-09 22:20:29 +1300

    Most project done

--- src/se701a1/MainRunner.java
+++ src/se701a1/MainRunner.java
@@ -1,5 +1,39 @@
 package se701a1;
 
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import med.*;
+
 public class MainRunner {
 
+	public static void main(String[] args) {
+		for (int i = 1; i <= 1; i++ ) {
+			String fil = "tests"+System.getProperty("file.separator")+"input"+i+".txt";
+			File f = new File(fil);
+			try {
+				FileInputStream is = new FileInputStream(f);
+				MedParser parser = new MedParser(is);
+				
+				try {
+					parser.MedCenter();
+					System.out.println(f.getName()+" OK");
+					System.out.println("------------------------------------");
+				} catch (TokenMgrError e) {
+					System.err.print(f.getName()+" failed in the Scanner: ");
+					System.err.println(e.getMessage());;
+					System.err.println("------------------------------------");
+				} catch (ParseException e) {
+					System.err.print(f.getName()+" failed in the Parser: ");
+					System.err.println(e.getMessage());;
+					System.err.println("------------------------------------");
+				}
+			} catch (FileNotFoundException e) {
+				System.err.println("Cannot find "+f.getName());
+				System.err.println("------------------------------------");
+			}
+		}
+	}
 }
